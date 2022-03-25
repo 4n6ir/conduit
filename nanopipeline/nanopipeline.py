13c289a0960ca69e8291110f8fa9831fba2b3805
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-03-25 01:37:42 +0000

    cdk

--- nanopipeline/nanopipeline.py
+++ nanopipeline/nanopipeline.py
@@ -1,7 +1,5 @@
 import boto3
 import json
-import os
-import time
 import zipfile
 from subprocess import run
 
@@ -10,7 +8,21 @@ def handler(event, context):
     p = run( [ 'cdk', 'version' ], capture_output = True )
     print("AWS Cloud Development Kit (CDK)", p.stdout.decode())
     
-    os.system('df -h')
+    s3 = boto3.client('s3')
+    s3.download_file(os.environ['BUCKET'], event['bundle'], '/tmp/'+event['bundle'])
+
+    with zipfile.ZipFile('/tmp/'+event['bundle'], 'r') as z:
+        z.extractall('/tmp')
+
+    if event['type'] == 'deploy':
+        
+        p = run( [ 'cd', '/tmp/'+event['bundle'], '&&', 'cdk', 'deploy', '--all', '--require-approval', 'never' ], capture_output = True)
+        print(p.stdout.decode())
+
+    if event['type'] == 'destroy':
+    
+        p = run( [ 'cd', '/tmp/'+event['bundle'], '&&', 'cdk', 'destroy', '--all', '--force' ], capture_output = True)
+        print(p.stdout.decode())
 
     return {
         'statusCode': 200,
