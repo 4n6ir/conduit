12fb8e885746c6a7ebb88552a2f9e19c34c39df0
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-03-25 02:42:58 +0000

    path

--- nanopipeline/nanopipeline.py
+++ nanopipeline/nanopipeline.py
@@ -12,20 +12,20 @@ def handler(event, context):
     s3 = boto3.client('s3')
     s3.download_file(os.environ['BUCKET'], event['bundle'], '/tmp/'+event['bundle'])
 
+    os.system('mkdir -p /tmp/'+event['bundle'][:-4])
+    
     with zipfile.ZipFile('/tmp/'+event['bundle'], 'r') as z:
-        z.extractall('/tmp')
-
-    os.system('cd /tmp | ls -lh')
-
+        z.extractall('/tmp'+event['bundle'][:-4])
+    
     if event['type'] == 'deploy':
         
-        command = 'cd /tmp/'+event['bundle']+' && cdk deploy --all --require-approval never' 
+        command = 'cd /tmp/'+event['bundle'][:-4]+' && cdk deploy --all --require-approval never' 
         p = run( [ command ], capture_output = True)
         print(p.stdout.decode())
 
     if event['type'] == 'destroy':
     
-        command = 'cd /tmp/'+event['bundle']+' && cdk destroy --all --force'
+        command = 'cd /tmp/'+event['bundle'][:-4]+' && cdk destroy --all --force'
         p = run( [ command ], capture_output = True)
         print(p.stdout.decode())
 
