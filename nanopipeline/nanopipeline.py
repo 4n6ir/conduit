04808990a710e45368fccb619b21f91f72c9445a
John Lukach <84751182+jblukach@users.noreply.github.com>
2022-03-25 03:00:19 +0000

    ls

--- nanopipeline/nanopipeline.py
+++ nanopipeline/nanopipeline.py
@@ -12,22 +12,25 @@ def handler(event, context):
     s3 = boto3.client('s3')
     s3.download_file(os.environ['BUCKET'], event['bundle'], '/tmp/'+event['bundle'])
 
-    os.system('mkdir -p /tmp/'+event['bundle'][:-4])
-    
-    with zipfile.ZipFile('/tmp/'+event['bundle'], 'r') as z:
-        z.extractall('/tmp/'+event['bundle'][:-4])
-    
-    if event['type'] == 'deploy':
-        
-        command = 'cd /tmp/'+event['bundle'][:-4]+' && cdk deploy --all --require-approval never' 
-        p = run( [ command ], capture_output = True)
-        print(p.stdout.decode())
+    print(os.system('cd /tmp && ls'))    
 
-    if event['type'] == 'destroy':
+
+    #os.system('mkdir -p /tmp/'+event['bundle'][:-4])
     
-        command = 'cd /tmp/'+event['bundle'][:-4]+' && cdk destroy --all --force'
-        p = run( [ command ], capture_output = True)
-        print(p.stdout.decode())
+    #with zipfile.ZipFile('/tmp/'+event['bundle'], 'r') as z:
+    #    z.extractall('/tmp/'+event['bundle'][:-4])
+    
+    #if event['type'] == 'deploy':
+        
+    #    command = 'cd /tmp/'+event['bundle'][:-4]+' && cdk deploy --all --require-approval never' 
+    #    p = run( [ command ], capture_output = True)
+    #    print(p.stdout.decode())
+
+    #if event['type'] == 'destroy':
+    
+    #    command = 'cd /tmp/'+event['bundle'][:-4]+' && cdk destroy --all --force'
+    #    p = run( [ command ], capture_output = True)
+    #    print(p.stdout.decode())
 
     return {
         'statusCode': 200,
